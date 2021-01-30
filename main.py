d617b06ecee873734bbc71010546040c84f87daf
Zachary Kerr <zacharykerr@ufl.edu>
2021-01-30 14:17:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import pygame
 
-pygame.init()
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# player has a board, boards have tiles, tiles have types
 
-screen = pygame.display.set_mode((800, 600))
 
-running = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
+def drawPlayerBoard():
+    tileSize = 20
+    for x in range(20, 120, 20):
+        for y in range(20, 120, 20):
+            rect = pygame.Rect(x, y, tileSize, tileSize)
+            pygame.draw.rect(screen, WHITE, rect, 1)
+
+
+def main():
+    global screen
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    screen.fill(BLACK)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        pygame.display.update()
+
+main()
